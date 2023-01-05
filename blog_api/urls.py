e7a79cfb427d65831f34d24e50a360ed464adeea
Najah <najahmatsuo7@gmail.com>
2023-01-06 02:07:09 +0900

    Viewの最初の実装、URLConfig、動作確認OK

--- blog_api/urls.py
+++ blog_api/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-
+    # path('',)
+    path('list/', views.BlogPostListAPIView.as_view()),
+    path('retrieve/<str:pk>', views.BlogPostRetrieveAPIView.as_view()),
+    path('retrieve_update/<str:pk>', views.BlogPostRetrieveUpdateAPIView.as_view()),
+    path('create/', views.BlogPostCreateAPIView.as_view()),
+    path('update/', views.BlogPostUpdateAPIView.as_view()),
+    path('delete/', views.BlogPostDestroyAPIView.as_view())
 ]
--- blog_api/views.py
+++ blog_api/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from rest_framework import generics
+from django_filters import rest_framework as filters
+
+from .serializers import BlogPostSerializer
+from .models import BlogPost
+
 
 # Create your views here.
+
+
+class BlogPostCreateAPIView(generics.CreateAPIView):
+    """記事作成用のAPIクラス"""
+
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+
+
+class BlogPostListAPIView(generics.ListAPIView):
+    """投稿投稿記事の取得（一覧）APIクラス"""
+
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+    filter_backends = [filters.DjangoFilterBackend]
+    filterset_fields = '__all__'
+
+
+class BlogPostRetrieveAPIView(generics.RetrieveAPIView):
+    """投稿投稿記事の取得（詳細）APIクラス"""
+
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+
+
+class BlogPostRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+
+
+class BlogPostUpdateAPIView(generics.UpdateAPIView):
+    """投稿記事の更新、一部更新APIクラス"""
+
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+
+
+class BlogPostDestroyAPIView(generics.DestroyAPIView):
+    """投稿記事の削除APIクラス"""
+
+    queryset = BlogPost.objects.all()
