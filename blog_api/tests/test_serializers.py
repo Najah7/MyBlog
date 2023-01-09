5f91e6cc5830ae8443047cdc51452b789eeac136
Najah <najahmatsuo7@gmail.com>
2023-01-09 21:53:35 +0900

    最初の実装のシリアライザのテストの追加

---
+++ blog_api/tests/test_serializers.py
@@ -0,0 +1,29 @@
+import uuid
+
+from django.test import TestCase
+from django.utils.timezone import localtime
+
+from ..models import BlogPost
+from ..serializers import BlogPostSerializer
+
+class TestBlogPostSerializer(TestCase):
+    """BlogPostSerializerのテスト"""
+
+    def test_input_vaild(self):
+        """入力データのバリデーション（OK）"""
+
+        input_data = {
+            "id": uuid.uuid4(),
+            "title": "test",
+            "article": "test",
+            # イメージフィールドのところだけシリアライザをどうテストするのかわからず、なので一度放置する。
+            # "thumbnail": "http://127.0.0.1:8000/media/images/NYC_times_square.png",
+            "short_description": "test",
+            "created_at": "2023-01-06T23:02:31.064472+09:00",
+            "updated_at": "2023-01-06T23:02:29+09:00",
+            "n_goods": 0
+        }
+
+        serializer = BlogPostSerializer(data=input_data)
+
+        self.assertTrue(serializer.is_valid())
